f3b3d6b18424df130cee91eaaf87f80ba1873c2c
ceccs17d57 <43899059+ceccs17d57@users.noreply.github.com>
2019-09-03 07:34:11 +0530

    Add files via upload

---
+++ vishnu.c
@@ -0,0 +1,168 @@
+#include<stdio.h>
+void waitingtime(int wt[],int bt[],int n)
+{
+	int i,j;
+	float avgwt=0;
+	wt[0]=0;
+	for(i=1;i<n;i++)
+	{
+		wt[i]=wt[i-1]+bt[i-1];
+	}
+	for(i=0;i<n;i++)
+	{
+		avgwt += wt[i];
+	}
+	printf("average waiting time is %f \n",(avgwt/n));
+}
+void turnaroundtime(int wt[],int bt[],int n)
+{
+	int i,tt[20];
+	float avgtt=0;
+	for(i=0;i<n;i++)
+	{
+		tt[i]=wt[i]+bt[i];
+		avgtt=avgtt+tt[i];
+	}
+	printf("average tt %f\n", (avgtt/n));
+}
+void roundrobin(int bt[],int rt[],int n,int timequantum)
+{
+int i=0,flag=0;
+float waiting_time=0,turnaround_time=0;
+int remain=n;
+int time=0;
+for (i=0; i<n; i++) {
+	printf("bt: %d\trt: %d\n", bt[i], rt[i]);
+}
+for(time=0,i=0;remain!=0;)
+{
+	if(rt[i]<=timequantum && rt[i]>0)
+	{
+		printf("r[i] < timequantum\n");
+		time=time+rt[i];
+		rt[i]=0;
+		flag=1;
+	}
+	else if(rt[i]>0)
+	{
+		printf("r[i] > 0\n");
+		time=time+timequantum;
+		rt[i]=rt[i]-timequantum;
+	}
+	if(rt[i]==0 && flag==1)
+	{
+		remain--;
+		waiting_time+=time-bt[i];
+		turnaround_time+=time;
+		flag=0;
+	}
+	if(i==n-1)
+	{
+		i=0;
+	}
+	else
+		i++;
+}
+printf("\naverage waiting time= %f\n",waiting_time/n);
+printf(" average tt=%f\n",turnaround_time/n);
+}
+
+void sortpriority(int bt[],int n,int p[])
+{	
+	int i,j,temp;
+	for(i=0;i<n-1;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(p[i]>p[j])
+			{
+				temp=p[i];
+				p[i]=p[j];
+				p[j]=temp;
+
+				temp=bt[i];
+				bt[i]=bt[j];
+				bt[j]=temp;
+			}
+		}
+	}
+}
+	
+void sort(int bt[],int n)
+{
+	int i,j,temp;
+	for(i=0;i<n-1;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(bt[i]>bt[j])
+			{
+				temp=bt[i];
+				bt[i]=bt[j];
+				bt[j]=temp;
+			}
+		}
+	}
+}
+void fcfs(int wt[],int bt[],int n)
+{
+	waitingtime(wt,bt,n);
+	turnaroundtime(wt,bt,n);
+}
+void sjf(int wt[],int bt[],int n)
+{
+	sort(bt,n);
+	waitingtime(wt,bt,n);
+	turnaroundtime(wt,bt,n);
+}
+
+void priority(int wt[],int bt[],int n,int p[])
+{
+	sortpriority(bt,n,p);
+	waitingtime(wt,bt,n);
+	turnaroundtime(wt,bt,n);
+}
+
+
+
+void main()
+{
+	int y=1,choice,bt[20],wt[20],n,i,tt[20],p[20],rt[20],timequantum;
+	printf("enter the no of process");
+	scanf("%d",&n);
+	printf("enter the burst time and priority");
+	for(i=0;i<n;i++)
+	{
+		printf("p[%d]:",i+1);
+		scanf("%d",&bt[i]);
+		scanf("%d",&p[i]);
+		rt[i]=bt[i];
+	}
+	printf("enter the time quantum");
+	scanf("%d",&timequantum);
+	
+	while(y=1)
+	{
+		printf("1.fcfs");
+		printf("2.sjf");
+		printf("3.round robin");
+		printf("4.priority");
+
+		printf("enter the choice");
+		scanf("%d",&choice);
+		switch (choice){
+			case 1: fcfs(wt,bt,n);
+				break;
+			case 2: sjf(wt,bt,n);
+				break;
+			case 3: roundrobin(bt,rt,n,timequantum);
+				break;
+			case 4: priority(wt,bt,n,p);
+				break;
+			default: printf("enter the right choice");
+				 break;
+		}
+	}
+}
+
+
